fda14bf0cdfe2eeca0d84155c2175811874d8f5f
devToms <tomaszkedziora14@example.com>
2025-02-13 00:50:19 +0100

    removing and adding valid fields

--- src/Module/Commerce/Application/DTO/RemoveItemFromCartDTO.php
+++ src/Module/Commerce/Application/DTO/RemoveItemFromCartDTO.php
@@ -10,29 +10,33 @@ use OpenApi\Attributes as OA;
 
 class RemoveItemFromCartDTO
 {
+    #[Assert\NotBlank(message: 'Cart UUID cannot be empty.')]
+    #[Assert\Uuid(message: 'Invalid Cart UUID format.')]
+    #[OA\Property(type: 'string', format: 'uuid', description: 'UUID of the cart')]
+    #[Groups(['default'])]
+    public readonly string $cartUuid;
+
+
     #[Assert\NotBlank(message: 'Product ID cannot be empty.')]
-    #[OA\Property(type: 'integer', description: 'ID of the product')]
+    #[OA\Property(type: 'string', description: 'ID of the product')]
     #[Groups(['default'])]
     public readonly int $productId;
 
+ 
     #[Assert\NotBlank(message: 'Quantity is required.')]
     #[Assert\Positive(message: 'Quantity must be a positive number.')]
-    #[OA\Property(type: 'integer', description: 'Quantity of the product to remove')]
+    #[OA\Property(type: 'integer', description: 'Quantity of the product')]
     #[Groups(['default'])]
     public readonly int $quantity;
-
-    #[Assert\Uuid(message: 'Invalid user ID format.')]
-    #[OA\Property(type: 'string', format: 'uuid', description: 'Optional user ID')]
-    #[Groups(['default'])]
-    public readonly ?string $userId;
-
+    
+    
     public function __construct(
+        string $cartUuid,
         int $productId,
         int $quantity,
-        ?string $userId = null
     ) {
+        $this->cartUuid = $cartUuid;
         $this->productId = $productId;
         $this->quantity = $quantity;
-        $this->userId = $userId;
     }
 }
